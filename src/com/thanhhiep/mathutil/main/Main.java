48117800dbf979d60b1cbab3306a16a7a51acb52
Thanh-hiep-NT-0105 <92801323+get-go-now@users.noreply.github.com>
2022-05-24 09:14:40 +0700

    update code directly on server

--- src/com/thanhhiep/mathutil/main/Main.java
+++ src/com/thanhhiep/mathutil/main/Main.java
@@ -19,16 +19,9 @@ public class Main {
         System.out.println("0! = 1? " + MathUtil.getFactorial(0));
         System.out.println("1! = 1? " + MathUtil.getFactorial(1));
         System.out.println("5! = 120? " + MathUtil.getFactorial(5));
-
+        System.out.println("6! = 720? " + MathUtil.getFactorial(6)); // modify by someone before 
     }
-    //modify from local again
-    public static void testFactorialSuccessfullCases() {
-        System.out.println("1! = 1? " + MathUtil.getFactorial(0));
-        
-
-
-        System.out.println("6! = 720? " + MathUtil.getFactorial(6)); // modify by someone before again  approve
-    }
+  
 }
 
 // tự nhìn, tự so sánh, tự kết luận cho từng trường hợp và cho all
