95d6a349b67c3b4fdfc3b835c8c9f3af632e10e1
get-go-now <nguyenhiep0456@gmail.com>
2022-05-30 16:35:14 +0700

    use recursion to demonstrate Regression Testing & GREEN 4EVER

--- src/com/thanhhiep/mathutil/core/MathUtil.java
+++ src/com/thanhhiep/mathutil/core/MathUtil.java
@@ -23,20 +23,39 @@ public class MathUtil {
     // câu lệnh gọi hàm một chỗ thi nó gọi là DATA DRIVEN TESTING
     // TA NGHĨ VỀ TEST, KỊCH BẢN TEST, BỘ DỮ LIỆU TEST TRƯỚC KHI VIẾT CODE
     
-    public static long getFactorial(int n){
-         
-        if(n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0..20");
-        
+    //Ð? quy, recursion, g?i l?i chính mình v?i 1 quy mô khác
+    //con búp bê ngu?i Nga
+    // 5! = 1.2.3.4.5
+    //5! = 5 * 4!
+    //4! = 4 * 3!
+    //3! = 2 * 1!
+    //1! = 1, dung luôn
+    //n! = n * ( n -1)! công thuc de quy lap lai chính mình v?i quy mô khác
+    public static long getFactorial(int n) {
+        if(n < 0 || n > 21)
+            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
         if(n == 0 || n == 1)
-            return 1;
-        long product =  1;
+            return 1; // di?u kien dùng c?a d? quy
+        return n * getFactorial(n - 1); // done de quy
         
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-       
-        return product; 
     }
+    
+    
+    
+//    public static long getFactorial(int n){
+//         
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        
+//        if(n == 0 || n == 1)
+//            return 1;
+//        long product =  1;
+//        
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//       
+//        return product; 
+//    }
     
     // y = f(x) = x^2
     // f(5) = 25
